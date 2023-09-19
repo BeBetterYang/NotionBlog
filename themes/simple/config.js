fa229397b4efe8785687154935f9821f0b5f13c3
BeBetterYang <145413327+BeBetterYang@users.noreply.github.com>
2023-09-19 23:34:06 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -7,9 +7,9 @@ const CONFIG = {
   AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
   // 菜单配置
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true // 显示搜索
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
+  MENU_SEARCH: false // 显示搜索
 }
 export default CONFIG
